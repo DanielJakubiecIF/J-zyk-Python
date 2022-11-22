c30382c4c9ea2f3a6fb2c9a82d18e5eafb2305ec
Daniel Jakubiec ZFM UJ <116658662+DanielJakubiecIF@users.noreply.github.com>
2022-11-22 20:25:49 +0100

    Update Zestaw 4.py

--- Zestaw 4.py
+++ Zestaw 4.py
@@ -10,5 +10,51 @@ def factorial(n):
     return result
 
 
+print(factorial(5))
+
 4.4
 
+def fibonacci(n):
+    if n < 2:
+        return n
+    n1, n0 = 0, 1
+    for i in range(1, n):
+        n1, n0 = n0, n1 + n0
+    return n0
+
+
+print(fibonacci(10))
+
+4.5
+
+def odwracanie(L, left, right):
+    left += len(L) if left < 0 else 0
+    right += len(L) if right < 0 else 0
+    while left < right:
+        L[left], L[right] = L[right], L[left]
+        left = left+1
+        right = right-1
+
+
+4.6
+
+def sum_seq1(sequence1):
+    result = 0
+    for item in sequence1:
+        if isinstance(item, (list, tuple)):
+            result += item
+    return result
+
+4.7
+
+seq = [1,(2,3),[],[4,(5,6,7)],8,[9]]
+def flatten(seq):
+    alist = []
+    for item in seq:
+        if isinstance(item, (list, tuple)):
+            alist.extend(flatten(item))
+        else:
+            alist.append(item)
+    return alist
+
+print(flatten(seq))
