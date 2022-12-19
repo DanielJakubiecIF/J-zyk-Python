29ff3c7766a1d79b5726f9c4c945b78a76e5a2df
Daniel Jakubiec ZFM UJ <116658662+DanielJakubiecIF@users.noreply.github.com>
2022-12-19 18:13:48 +0100

    Delete 2.17.py

--- 2.17.py
+++
@@ -1,7 +0,0 @@
-line = "abc def ghijk  lmonp r s tuwxyz GvR"
-help(sorted)
-wyrazy = line.split()
-sorted(wyrazy, key=None, reverse=False)
-['GvR', 'abc', 'def', 'ghijk', 'lmonp', 'r', 's', 'tuwxyz']
-sorted(wyrazy, key=len, reverse=False)
-['r', 's', 'abc', 'def', 'GvR', 'ghijk', 'lmonp', 'tuwxyz']
