71e02edc0291e9b4cc774634ef1c71e2262cec7d
Daniel Jakubiec ZFM UJ <116658662+DanielJakubiecIF@users.noreply.github.com>
2022-12-19 18:13:33 +0100

    Delete 2.14.py

--- 2.14.py
+++
@@ -1,8 +0,0 @@
-line = "abc def ghijk lmonp r s tuwxyz"
-longest =  max(line.split(), key=len)
-print("longest word is: ",  longest)
-longest word is:  tuwxyz
-line.split()
-['abc', 'def', 'ghijk', 'lmonp', 'r', 's', 'tuwxyz']
-len(longest)
-6
