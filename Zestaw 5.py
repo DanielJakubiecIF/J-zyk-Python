e3be438bef9abad8fdc5413045c1394a30f8af71
Daniel Jakubiec ZFM UJ <116658662+DanielJakubiecIF@users.noreply.github.com>
2022-12-19 18:08:08 +0100

    Update Zestaw 5.py

--- Zestaw 5.py
+++ Zestaw 5.py
@@ -55,17 +55,22 @@ class TestFractions(unittest.TestCase):
     def test_add_frac(self):
         self.assertEqual(add_frac([1, 2], [1, 3]), [5, 6])
 
-    def test_sub_frac(self): pass
+    def test_sub_frac(self):
+        self.assertEqual (sub_frac([1, 2], [1, 3]), [1, 6])
 
-    def test_mul_frac(self): pass
+    def test_mul_frac(self):
+        self.assertEqual (mul_frac([1, 2], [1, 3]), [1, 6])
 
-    def test_div_frac(self): pass
+    def test_div_frac(self):
+        self.assertEqual (div_frac([1, 2], [1, 3]), [3, 2])
 
-    def test_is_positive(self): pass
+    def test_is_positive(self):
+        self.assertEqual (is_positive([1, 2]), 1)
 
-    def test_is_zero(self): pass
+    def test_is_zero(self):
+        self.assertEqual (is_zero([0, 2]), 1)
 
-    def  test_cmp_frac(self):
+    def test_cmp_frac(self):
         self.assertTrue(is_positive([5,6]))
         self.assertFalse(is_positive([-5,6]))
         self.assertTrue(is_zero([0, 6]))
@@ -76,7 +81,8 @@ class TestFractions(unittest.TestCase):
         self.assertEqual(cmp_frac([1, 6], [1, 3]), -1)
 
 
-    def test_frac2float(self): pass
+    def test_frac2_float(self):
+        self.assertAlmostEqual(frac2_float([1, 2]),  0.5)
 
     def tearDown(self): pass
 
